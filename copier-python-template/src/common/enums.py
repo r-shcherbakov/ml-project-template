92b4e43475ec1e01c0304055410a77256785d810
Shcherbakov, Roman <Roman.Shcherbakov@nedra.digital>
2024-04-04 18:01:43 +0300

    feat: added pipeline steps enum

--- copier-python-template/src/common/enums.py
+++ copier-python-template/src/common/enums.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 """Module defines enums."""
+from common.pipeline_steps import (
+    PRE_RUN,
+    PREPROCESS,
+    FEATURE_ENGINEER,
+    SELECT_FEATURES,
+    SPLIT_DATASET,
+    TRAIN,
+    PLOTTING,
+    HYPERPARAMETER_OPTIMIZATION,
+    POST_RUN,
+)
 
 
 class PipelineSteps:
     """
     Enum for pipeline steps.
     """
-
-    pre_run = 'pre_run'
-    preprocess = 'preprocess'
-    feature_engineer = 'feature_engineer'
-    select_features = 'select_features'
-    split_dataset = 'split_dataset'
-    train = 'train'
-    plotting = 'plotting'
-    hyperparameter_optimization = 'hyperparameter_optimization'
-    post_run = 'post_run'
+    pre_run = PRE_RUN
+    preprocess = PREPROCESS
+    feature_engineer = FEATURE_ENGINEER
+    select_features = SELECT_FEATURES
+    split_dataset = SPLIT_DATASET
+    train = TRAIN
+    plotting = PLOTTING
+    hyperparameter_optimization = HYPERPARAMETER_OPTIMIZATION
+    post_run = POST_RUN
